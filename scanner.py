8c1fa151adb8626e4003ae9b5c271c2405821ee5
Jonathan Vandionant <jonathan@MBPvanJonathan.fritz.box>
2021-11-05 15:37:46 +0100

    WIP

--- scanner.py
+++ scanner.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+import streamlit as st
 
 def decoder(ret, image):
     gray_img = cv2.cvtColor(image,0)
@@ -19,16 +20,36 @@ def decoder(ret, image):
         
         cv2.putText(image, string, (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255), 2)
 
-        if ret and frame is not None:
-            cv2.imwrite('img.jpg', frame)
+        if ret and image is not None:
+            cv2.imwrite('img.jpg', image)
 
         print("Barcode: "+barcodeData +" | Type: "+barcodeType)
 
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
+
+# st.title("Webcam Live Feed")
+# run = st.checkbox('Run')
+# FRAME_WINDOW = st.image([])
+# camera = cv2.VideoCapture(0)
+
+# while run:
+#     ret, frame = camera.read()
+#     decoder(ret, frame)
+#     cv2.imshow('Image', frame)
+#     code = cv2.waitKey(10)
+#     if code == ord('q'):
+#         break
+# else:
+#     st.write('Stopped')
+
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    ret, frame = camera.read()
     decoder(ret, frame)
-    cv2.imshow('Image', frame)
-    code = cv2.waitKey(10)
-    if code == ord('q'):
-        break
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
